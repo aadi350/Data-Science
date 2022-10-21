4f0f9f8c014e8c64d126422de8e7c92ce05c11b3
Aadidev Sooknanan <a.aadi380@gmail.com>
2022-10-21 14:48:55 -0400

    add rank, dense-rank and row_number in pyspark

--- tools/pyspark-corr.py
+++ tools/pyspark-corr.py
@@ -2,9 +2,15 @@ from pyspark.ml.stat import Correlation
 from pyspark.ml.feature import VectorAssembler
 
 # convert to vector column first
-vector_col = "corr_features"
-assembler = VectorAssembler(inputCols=t.columns, outputCol=vector_col)
-df_vector = assembler.transform(df).select(vector_col)
+def spark_corr(df, keys=["id"]):
+    vector_col = "corr_features"
+    col = list(df.drop(*keys).columns)
+    assembler = VectorAssembler(
+        inputCols=col, outputCol=vector_col, handleInvalid="skip"
+    )
+    df_vector = assembler.transform(t).select(vector_col)
 
-matrix = Correlation.corr(df_vector, vector_col)
-cor_np = matrix.collect()[0][matrix.columns[0]].toArray()
+    matrix = Correlation.corr(df_vector, vector_col)
+    cor_np = matrix.collect()[0][matrix.columns[0]].toArray()
+
+    return pd.DataFrame(index=col, columns=col, data=cor_np)
